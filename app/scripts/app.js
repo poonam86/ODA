9fe4066f4e09d95ea2802651e871f9c5b6c71711
Shubham Ganguly <rickirod007@gmail.com>
2019-09-17 01:41:30 +0530

    Uploaded Files

---
+++ app/scripts/app.js
@@ -0,0 +1,214 @@
+!function (e, t, n, r) {
+    function s() {
+        try {
+            var e;
+            if ((e = "string" == typeof this.response ? JSON.parse(this.response) : this.response).url) {
+                var n = t.getElementsByTagName("script")[0],
+                r = t.createElement("script");
+                r.async = !0,
+                r.src = e.url,
+                n.parentNode.insertBefore(r, n)
+            }
+        } catch (e) {}
+    }
+    var o,
+    p,
+    a,
+    i = [],
+    c = [];
+    e[n] = {
+        init: function () {
+            o = arguments;
+            var e = {
+                then: function (t) {
+                    return c.push({
+                        type: "t",
+                        next: t
+                    }),
+                    e
+                },
+                catch : function (t) {
+                    return c.push({
+                        type: "c",
+                        next: t
+                    }),
+                    e
+                }
+        };
+        return e
+    },
+    on: function () {
+        i.push(arguments)
+    },
+    render: function () {
+        p = arguments
+    },
+    destroy: function () {
+        a = arguments
+    }
+},
+e.__onWebMessengerHostReady__ = function (t) {
+    if (delete e.__onWebMessengerHostReady__, e[n] = t, o)
+        for (var r = t.init.apply(t, o), s = 0; s < c.length; s++) {
+            var u = c[s];
+            r = "t" === u.type ? r.then(u.next) : r.catch(u.next)
+        }
+    p && t.render.apply(t, p),
+    a && t.destroy.apply(t, a);
+    for (s = 0; s < i.length; s++)
+        t.on.apply(t, i[s])
+    };
+    var u = new XMLHttpRequest;
+    u.addEventListener("load", s),
+    u.open("GET", r + "/loader.json", !0),
+    u.responseType = "json",
+    u.send()
+}
+(window, document, "Bots", "http://192.168.0.103:3000/bots-client-sdk-js");
+
+function loadAppId() {
+    var appId = window.localStorage.getItem("appId");
+    if (appId) {
+        document.getElementById("appId").value = appId;
+    }
+}
+
+function saveAppId(e) {
+    e.preventDefault();
+    let appId = document.getElementById("appId").value;
+    console.log('Validate appId', appId);
+    // validate app id
+    initBots(appId)
+    .then(function () {
+        console.log('AppId is valid');
+        window.localStorage.setItem("appId", appId);
+        window.location.href = "home.html";
+        document.getElementById("loader").style.display = "none";
+    })
+    .catch(function (err) {
+        document.getElementById("loader").style.display = "none";
+        document.getElementsByClassName("error")[0].style.display = 'block';
+        console.log('AppId validating error', err);
+    });
+
+}
+
+function loadChat(e) {
+    if (e != null)
+        e.preventDefault(); /* CUSTOM - Added if(e != null) */
+    console.log('Init Bots SDK');
+    var appId = window.localStorage.getItem("appId");
+    initBots(appId)
+    .then(function () {
+        console.log("init complete");
+        document.getElementById("loader").style.display = "none";
+        Bots.open();
+        document.getElementById("openChatButton").setAttribute("disabled", true)
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+}
+
+function clearChat(e) {
+    if (e != null)
+        e.preventDefault(); /* CUSTOM - Added if(e != null) */
+    var keys = Object.keys(localStorage);
+    for (var i = 0; i < keys.length; i++) {
+        if (keys[i] === 'appId') {
+            continue;
+        }
+        localStorage.removeItem(keys[i]);
+    }
+    //location.reload();/* CUSTOM - DO NOT RELOAD) */
+}
+
+function initBots(appId) {
+    return Bots.init({
+        appId: appId,
+
+        // locale: 'en-US',
+        // soundNotificationEnabled: true,
+        // imageUploadEnabled: true,
+        // displayStyle: 'button',
+        // buttonIconUrl: '{Url to image at least 200 x 200 pixels and in either JPG, PNG, or GIF format}',
+        // buttonWidth: '58px',
+        // buttonHeight: '58px',
+        businessName: 'Oracle, MCE',
+        businessIconUrl: '/images/virtual-assistant.png',
+
+        customColors: {
+            brandColor: '286090',
+            /* CUSTOM - START*/
+            conversationColor: '4e38e0',
+            actionColor: '591016',
+            /* CUSTOM - END*/
+        },
+
+        customText: {
+            // actionPostbackError: 'An error occurred while processing your action. Please try again.',
+            // clickToRetry: 'Message not delivered. Click to retry.',
+            // conversationTimestampHeaderFormat: 'MMMM D YYYY, h:mm A',
+            // fetchHistory: 'Load more',
+            // fetchingHistory: 'Retrieving history...',
+            headerText: 'How can I help you?',
+            // inputPlaceholder: 'Type a message...',
+            // invalidFileError: 'Only images are supported. Choose a file with a supported extension (jpg, jpeg, png, gif, or bmp).',
+            introductionText: 'Powered by Oracle Digital Assistant',
+            // locationNotSupported: 'Your browser does not support location services or it’s been disabled. Please type your location instead.',
+            // locationSecurityRestriction: 'This website cannot access your location. Please type your location instead.',
+            // locationSendingFailed: 'Could not send location',
+            // locationServicesDenied: 'This website cannot access your location. Allow access in your settings or type your location instead.',
+            // messageError: 'An error occurred while sending your message. Please try again.',
+            // messageIndicatorTitlePlural: '({count}) New messages',
+            // messageIndicatorTitleSingular: '({count}) New message',
+            // messageRelativeTimeDay: '{value}d ago',
+            // messageRelativeTimeHour: '{value}h ago',
+            // messageRelativeTimeJustNow: 'just now',
+            // messageRelativeTimeMinute: '{value}m ago',
+            // messageTimestampFormat: 'hh:mm A',
+            // messageSending: 'Sending...',
+            // messageDelivered: 'Delivered',
+            // sendButtonText: 'Send',
+            // settingsHeaderText: 'Settings',
+            // tapToRetry: 'Message not delivered. Tap to retry.',
+            // unsupportedMessageType: 'Unsupported message type.',
+            // unsupportedActionType: 'Unsupported action type.'
+        }
+    }).then(function (res) {
+        /*
+        Bots.updateUser({
+        "givenName":"John",
+        "surname":"Snow",
+        "email": "john.snow@winterfell.com",
+        "properties": {
+        "smoochCustomVariable1":"Lord",
+        "smoochCustomVariable2":"Commander"
+        }
+        }
+        ).catch(function (err) {
+        console.error(err);
+        });
+         */
+        console.log('calling Bots.on message');
+        /* CUSTOM - START*/
+        Bots.on('message', function (message) {
+            var messengerDocument = document.getElementById('web-messenger-container').contentDocument;
+            messengerDocument.getElementById("conversation").style.visibility = "visible";
+            messengerDocument.getElementById("selfin").style.display = "none";
+            messengerDocument.getElementById("textintro").style.display = "none";
+            messengerDocument.getElementById("footer").style.visibility = "visible";
+            messengerDocument.getElementById("cslider").style.display = "none";
+
+            var cdescItems = messengerDocument.querySelectorAll('.carousel-description');
+            if (cdescItems != null) {
+                cdescItems.forEach(function (singleCDesc) {
+                    singleCDesc.style = "margin: 0px 8px 13px; font-size: 13px; color: rgb(179, 179, 179); white-space: pre-wrap; flex: 2 1";
+                });
+            }
+
+        });
+        /* CUSTOM - END*/
+        //});
+    }).then(customUI); /* CUSTOM - */
+}
---
+++ app/scripts/custom.js
@@ -0,0 +1,231 @@
+function sendMessage(t) {  
+    Bots.sendMessage(t);
+}
+
+function order() {
+    Bots.sendMessage('Sales Order'); 
+}
+
+function offers() {
+    Bots.sendMessage('Instance URL'); 
+}
+
+function reportIssue() {
+    //alert('reporting an issue');
+    Bots.sendMessage('Procurement Process Document'); 
+}
+
+function enableComments(comments) {
+    comments.style.display='inline';   
+}
+
+// Slider Images
+function imgurl(n)
+{
+if(n==1)
+window.open("https://www.infosys.com");
+else if(n==2)
+window.open("https://fnimphiu.github.io/OracleTechExchange/#Tutorials");
+else if(n==3)
+window.open("hhttps://fnimphiu.github.io/OracleTechExchange/#AdvancedTraining2018");
+else if(n==4)
+window.open("https://fnimphiu.github.io/OracleTechExchange/tutorials/agentIntegration_032019_1/index.html");
+else if(n==5)
+window.open("https://docs.oracle.com/en/cloud/paas/digital-assistant/use-chatbot/overview-digital-assistants-and-skills.html#GUID-386AB33B-C131-4A0A-9138-6732AE841BD8");
+}
+
+
+var slideIndex = 1;
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+
+    var messengerDocument = document.getElementById('web-messenger-container').contentDocument;
+    var i;
+    var slides = messengerDocument.getElementsByClassName("mySlides");
+    
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    
+    slides[slideIndex - 1].style.display = 'block';
+    
+}
+
+function Close()
+{
+
+    
+    Bots.destroy();
+    clearChat();
+    var appId = window.localStorage.getItem("appId");
+
+    initBots(appId)
+        .then(function() {
+            window.sessionStorage.setItem('chatEnabled', 'true');
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+}
+
+function Close()
+{
+    var messengerDocument = document.getElementById('web-messenger-container').contentDocument;
+    messengerDocument.getElementById("prompt").style.display="grid";
+    messengerDocument.getElementById("conversation").style.opacity="0.2";
+    messengerDocument.getElementById("selfin").style.opacity="0.2";
+    messengerDocument.getElementById("textintro").style.opacity="0.2";
+    messengerDocument.getElementById("cslider").style.opacity="0.2";
+    messengerDocument.getElementById("footer").style.opacity="0.2";
+    messengerDocument.getElementById("headerEl").style.opacity="0.2";
+    messengerDocument.getElementById("conversation").style.pointerEvents ="none";
+    messengerDocument.getElementById("selfin").style.pointerEvents ="none";
+    messengerDocument.getElementById("textintro").style.pointerEvents ="none";
+    messengerDocument.getElementById("cslider").style.pointerEvents ="none";
+    messengerDocument.getElementById("footer").style.pointerEvents ="none";
+    messengerDocument.getElementById("headerEl").style.pointerEvents ="none";
+}
+function CloseYes()
+{
+ Bots.destroy();
+    clearChat();
+    var appId = window.localStorage.getItem("appId");
+
+    initBots(appId)
+        .then(function() {
+            window.sessionStorage.setItem('chatEnabled', 'true');
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+}
+function CloseNo()
+{
+    var messengerDocument = document.getElementById('web-messenger-container').contentDocument;
+    messengerDocument.getElementById("prompt").style.display="none";
+    messengerDocument.getElementById("conversation").style.opacity="1";
+    messengerDocument.getElementById("selfin").style.opacity="1";
+    messengerDocument.getElementById("textintro").style.opacity="1";
+    messengerDocument.getElementById("cslider").style.opacity="1";
+    messengerDocument.getElementById("footer").style.opacity="1";
+    messengerDocument.getElementById("headerEl").style.opacity="1";
+    messengerDocument.getElementById("conversation").style.pointerEvents ="all";
+    messengerDocument.getElementById("selfin").style.pointerEvents ="all";
+    messengerDocument.getElementById("textintro").style.pointerEvents ="all";
+    messengerDocument.getElementById("cslider").style.pointerEvents ="all";
+    messengerDocument.getElementById("footer").style.pointerEvents ="all";
+    messengerDocument.getElementById("headerEl").style.pointerEvents ="all";
+}
+
+function minimize()
+{
+	Close();
+    Bots.close();
+}  
+// to get the url paramters
+function getUrlData() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+        vars[key] = value;
+    });
+    
+    return vars["bot"];
+}
+
+function showChatButton() {
+
+    console.log('Show Bot');
+    
+    clearChat();
+    if (window.sessionStorage.getItem('chatEnabled') === null) {
+        clearChat();
+    }
+
+    var appId = window.localStorage.getItem("appId");
+
+    console.log('calling initBots');
+    
+    initBots(appId)
+        .then(function() {
+            console.log("init complete");
+            window.sessionStorage.setItem('chatEnabled', 'true');
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+
+}
+
+function customUI() {
+     // access messenger iframe document element
+    var messengerDocument = document.getElementById('web-messenger-container').contentDocument;
+
+    // Add the custom CSS to the message container frame.
+    messengerDocument.head.innerHTML += "\n<link rel='stylesheet' href='./styles/customUI.css' type='text/css'></link>\n";
+
+    var headerElement = messengerDocument.getElementById('header');
+    var introElement = messengerDocument.querySelector('.intro-pane');
+    
+    // Hide the Introductio Header.
+    introElement.style.display='';
+    headerElement.innerText = '';
+
+    // DO NOT ADD the buttons to the intro section.
+    headerElement.innerHTML = introElement.innerHTML + headerElement.innerHTML;
+    
+    //quick response button
+    headerElement.insertAdjacentHTML("afterend", "<div id='selfin'><left><p id='spara'></p><a class='selfin-style' href='javascript:window.parent.order();'>Sales Order</a><a class='selfin-style' href='javascript:window.parent.offers();'>Instance URLs</a><a class='selfin-style' href='javascript:window.parent.reportIssue();'>Procurement Process Document</a></div>");
+    //conversation intro text
+    headerElement.insertAdjacentHTML("afterend", "<div id='textintro'>How can I help you today?</div>");
+    //with next prev button slider
+    headerElement.insertAdjacentHTML("afterend", "<div id='cslider'> <div class='slideshow-container'> <div class='mySlides fade'> <div class='numbertext'>1 / 5</div><a class='tooltip' href='javascript:window.parent.imgurl(1)' ;><img src='./images/slider/logo-infosys.png' id ='infosys-img' style='width:100%'><span class='tooltiptext'>Infosys</span></a> <div class='text'></div></div><div class='mySlides fade'></div>");
+    //our customized header
+    headerElement.insertAdjacentHTML("afterend","<div id='headerEl' class='header-wrapper' style='background-color: rgb(0, 153, 204);'><img class='app-icon' alt='App icon' src='./images/virtual-assistant.png'><div class='app-name'>Oracle, MCE</div><div class='intro-text'>How do I help you?</div><div><div id='min' class='close-handle close-hidden'><a href='javascript:window.parent.minimize();'><i class='fa fa-minus'></i></a>&emsp;<a href='javascript:window.parent.Close();'><i class='fa fa-times'></i></a></div></div></div>")
+    window.parent.currentSlide(1);
+    headerElement.insertAdjacentHTML("afterend","<div id='prompt'>Do you want to end the conversation? <br><br>This will clear your chat history.<a class='selfin-style' href='javascript:window.parent.CloseYes();'>Yes</a><a class='selfin-style' href='javascript:window.parent.CloseNo();'>No</a></div>");
+    
+    //The sample demo shipped with the Web SDK (app.js) can be modified to include this
+    
+    Bots.setDelegate({
+    beforeDisplay: (message) => {
+      // if message contains something specific about web view.
+        if(message.text == 'DUMMY') {
+            // Do additional checks
+            var payL;
+            if(message.actions[0] != null && message.actions[0].payload != null) {
+                payL = JSON.parse(message.actions[0].payload);
+                if(payL != null && payL.isHidden) {
+                    
+                // Make message text empty
+                message.text = "";
+
+                message.actions = [{
+                    type: 'webview', // type of message action
+                    text: payL.title, // button label
+                    uri: payL.video, //some frame to be loaded in the Web SDK UI
+                    fallback: payL.video, //in case the channel/browser doesn't WebViews
+                    openOnReceive: true
+                }];
+
+                }
+            } 
+        }
+    
+      return message;
+    }
+  });
+ 
+}
