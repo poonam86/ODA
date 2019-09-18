bcdbc9e7da606dce89a62c111d5275db0def4d04
Shubham Ganguly <rickirod007@gmail.com>
2019-09-18 15:18:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use(function(req, res, next) {
 var staticPath = path.join(__dirname, '/app');
 app.use(express.static(staticPath));
 
-app.listen(3000, function() {
-  console.log('listening on port 3000');
+app.listen(8080, function() {
+  console.log('listening on port 8080');
 });
